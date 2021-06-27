df1dfaee80f5fbe4f1a5118aa0a97bfae48c82de
SylvainStak <stackersap@gmail.com>
2021-06-27 03:17:43 +0100

    changed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders footer text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sylvain Afonso Developer Test/i);
   expect(linkElement).toBeInTheDocument();
 });
